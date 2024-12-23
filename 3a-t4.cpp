619156f33e2643e92415dea0d7cf943e9e536608
s0awesome <imeni333344@gmail.com>
2024-12-23 12:09:18 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,23 +1,14 @@
-#include<iostream> 
+#include<iostream>
 using namespace std;
 int main()
 {
-	int k, i = 1, sum_k = 0, number;
-again:
-	cout << " Enter the amount of integers to sum = "; cin >> k;
-	if (k <= 0)
-	{
-		cout << " Enter possible amount of integers";
-		goto again;
-	}
-	else
-	{
-		do {
-			cout << "Enter integer nr. " << i << ": ";
-			cin >> number;
-			sum_k += number; i++;
-		} while (i <= k);
-		cout << " The total sum of " << k << " integers is: " << sum_k;
-	}
-	return 0;
+int k, i = 1, sum_k = 0, number;
+cout << " Enter the amount of integers to sum = "; cin >> k;
+do {
+cout << "Enter integer nr. " << i << ": ";
+cin >> number;
+sum_k += number; i++;
+} while (i <= k);
+cout << " The total sum of " << k << " integers is: " << sum_k;
+return 0;
 }
